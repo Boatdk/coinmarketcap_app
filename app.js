d9625cb0646ec2bf3d31de3dc5fe690fb5ff222f
boat.panitarn <boat.panitarn@bitkub.com>
2021-03-19 13:19:24 +0700

    fix

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ const sleep = (ms) => {
 		// await costService(botRate)
 		// await history()
 		console.log('miss===>',{_date,_time})
-		if(_time[0]=='05' && _time[1]=='50'){
+		if(_time[0]=='07' && _time[1]=='25'){
 			await connectMongoDB()
 			console.log('hit===>',_time)
 			// const botRate = await BotRate.findOne({ 'time': yesterday[0] })
